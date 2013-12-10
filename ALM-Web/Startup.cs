f8eaeee3117e364475a12976eb8344b791f0d4ad
Nick Peachey <nick.peachey@gmail.com>
2013-12-10 15:36:19 +0000

    initial solution creation

---
+++ ALM-Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ALM_Web.Startup))]
+namespace ALM_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
